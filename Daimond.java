c4793af979bd42ac34ac879c1990916a8c2c8cd1
priyacode008 <priya.software008@gmail.com>
2026-05-23 18:06:33 +0530

    Daimond program

---
+++ Daimond.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Daimond{
+    public static void main(String args []){
+        Scanner sc=new Scanner (System.in);
+        int i,j,r,c;
+        System.out.println("enter the row");
+        r=sc.nextInt();
+        System.out.println("enter the column");
+        c=sc.nextInt();
+        for(i=1;i<=r;i++){
+            for(j=1;j<=c;j++){
+                if(i==1&& j==3)
+                    System.out.print("*");
+                else if(i==2&& (j==2||j==4))
+                    System.out.print("*");
+                else if(i==3&& (j==1||j==3||j==5))
+                    System.out.print("*");
+                else if(i==4&& (j==2||j==4))
+                    System.out.print("*");
+                else if(i==5&& j==3)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
