d0ac9d955a87490da843b60ec14b83f51fdc316b
priyacode008 <priya.software008@gmail.com>
2026-07-03 13:32:40 +0530

    java

--- Sandgloss.java
+++ Sandgloss.java
@@ -1,22 +1,21 @@
-public class Daimond{
-    public static void main(String[] args) {
-        
-        int n=5,i,j;
+public class Sandgloss{
+    public static void main(String args []){
+        int i,j,n=5;
 
-        for(i=1;i<=n;i++){
-            for(j=i;j<=n;j++){
+        for(i=n;i>=1;i--){
+            for(j=n;j>=i;j--){
                 System.out.print(" ");
             }
-            for(j=1;j<=2*i-1;j++){
+            for(j=1;j<=(2*i-1);j++){
                 System.out.print("*");
             }
             System.out.println();
         }
-        for(i=n-1;i>=1;i--){
-            for(j=n;j>=i;j--){
+         for(i=1;i<=n;i++){
+            for(j=i;j<=n;j++){
                 System.out.print(" ");
             }
-            for(j=1;j<=2*i-1;j++){
+            for(j=1;j<=(2*i-1);j++){
                 System.out.print("*");
             }
             System.out.println();
